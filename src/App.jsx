d6e0d691fad8811e087f9ee90df10140f31787f5
subbufreelancer <30551779+subbufreelancer@users.noreply.github.com>
2025-08-29 17:09:05 +0000

    Updated the styles for social icons to include a hover animation effect. and added a bounce animation to enhance user interaction.

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Karthik Ananthappan",
+  title: "Front End Developer",
+  email: "karthikananthappan@gmail.com",
+  gitHub: "lorem ipsum",
+  instagram: "lorem ipsum",
+  linkedIn: "lorem ipsum",
   medium: "",
-  twitter: "microsoft",
+  twitter: "lorem ipsum",
   youTube: "Code",
 };
 
